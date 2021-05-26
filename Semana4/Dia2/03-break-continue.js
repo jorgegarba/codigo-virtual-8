3352ce06a050b86122a433c8c0ec17e5319f47ab
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-05-25 21:25:13 -0500

    subiendo documentacion para la entrega final de proyectos

--- Semana4/Dia2/03-break-continue.js
+++ Semana4/Dia2/03-break-continue.js
@@ -2,6 +2,8 @@
  * SENTENCIA break vs continue
  * break => interrumpir todas las iteraciones que quedan
  * por darse en una estructura repetitiva
+ * continue => interrumpir la iteracion actual, y salta
+ * automáticamente a la siguiente iteración
  */
 
 let numeros = [3, 545, 3, -5, 34, 90, 2, 90, 0];
@@ -18,3 +20,15 @@ for (let i = 0; i < numeros.length; i++) {
   }
 }
 
+
+/**
+ * EJERCICIO: si un número es impar imprimirlo, y en caso
+ * contrario saltar a la siguiente iteración
+ */
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    continue;
+  }
+  console.log(numeros[i]);
+}
