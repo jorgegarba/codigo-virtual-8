cab1b43edc418d1ffa3ac466977a956670126b7d
Jorge Garnica <jorgegarba@gmail.coma>
2021-06-07 22:01:55 -0500

    planteamiento ejercicio peliculas con bootstrap 5

---
+++ Semana6/Dia1/proyecto/js/index.js
@@ -0,0 +1,11 @@
+import { peliculas } from './data.js';
+const contenedor = document.getElementById('contenedor');
+
+const base_url_imagenes = 'https://image.tmdb.org/t/p/w500';
+
+const dibujarPeliculas = () => {
+
+  
+
+};
+dibujarPeliculas();
