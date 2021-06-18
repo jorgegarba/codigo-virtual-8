46702c3b88b6fc2f98d8a42c5795d60bbd7f4093
Jorge Garnica <jorgegarba@gmail.coma>
2021-06-17 20:26:41 -0500

    primer proyecto en react

---
+++ Semana7/01-react-hello/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+const App = () => {
+	return (
+		<div>
+			<header>Mi header</header>
+			<main>El contenido de la aplicación</main>
+			<Footer />
+		</div>
+	);
+};
+
+export default App;
---
+++ Semana7/01-react-hello/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>Año del bicentenario</p>
+			<small>CodiGo - 2021 - Todos los derecho reservados</small>
+		</footer>
+	);
+};
+export default Footer;
