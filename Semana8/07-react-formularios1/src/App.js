9048634fe7dd8e05d05351a906f1e26a9ce04df8
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-06-25 22:07:06 -0500

    ejercicio renderizando múltiples checkboxes

--- Semana8/07-react-formularios1/src/App.js
+++ Semana8/07-react-formularios1/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import FormsOnChange3 from './pages/FormsOnChange3';
+import FormsOnChange4 from './pages/FormsOnChange4';
 
+// import FormsOnChange3 from './pages/FormsOnChange3';
 // import FormsOnChange2 from './pages/FormsOnChange2';
 // import FormsOnChange from './pages/FormsOnChange';
 // import FormsUseRef from './pages/FormsUseRef';
@@ -11,7 +12,8 @@ const App = () => {
 			{/* <FormsUseRef /> */}
 			{/* <FormsOnChange /> */}
 			{/* <FormsOnChange2 /> */}
-			<FormsOnChange3 />
+			{/* <FormsOnChange3 /> */}
+			<FormsOnChange4 />
 		</div>
 	);
 };
