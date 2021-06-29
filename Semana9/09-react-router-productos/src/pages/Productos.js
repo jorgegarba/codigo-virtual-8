f01d5e67b9b635bceb8228732b72505a19fab063
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-06-28 22:05:49 -0500

    Creación de sidebar y enrutamiento para mini-proyecto ecommerce

---
+++ Semana9/09-react-router-productos/src/pages/Productos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Productos = () => {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+			accusantium necessitatibus incidunt reiciendis aut inventore dolor
+			corrupti saepe atque enim quaerat! Architecto sit dolores aliquam debitis
+			doloremque tempora, quam incidunt!
+		</div>
+	);
+};
+
+export default Productos;
