89a2ebb33920a2559aeaf9020314a1728c97909a
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-06-28 20:33:06 -0500

    Enviando y leyendo parametros a través de la URL con react-router-dom

--- Semana9/08-react-router/src/App.js
+++ Semana9/08-react-router/src/App.js
@@ -1,53 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
 import Contacto from './pages/Contacto';
+import Error404 from './pages/Error404';
+import Home from './pages/Home';
 import Nosotros from './pages/Nosotros';
 import Portafolio from './pages/Portafolio';
+import ProyectoDetalle from './pages/ProyectoDetalle';
 
 const App = () => {
 	return (
 		<Router>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<div className="container-fluid">
-					<a className="navbar-brand" href="#">
-						Navbar
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<Link to="/nosotros" className="nav-link">
-									Nosotros
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link to="/portafolio" className="nav-link">
-									Portafolio
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link to="/contacto" className="nav-link">
-									Contacto
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+			<Header />
 			<Switch>
 				<Route path="/nosotros" component={Nosotros} />
+				<Route path="/portafolio/:proyecto_id" component={ProyectoDetalle} />
 				<Route path="/portafolio" component={Portafolio} />
 				<Route path="/contacto" component={Contacto} />
+				<Route path="/" exact component={Home} />
+				<Route component={Error404} />
 			</Switch>
 		</Router>
 	);
